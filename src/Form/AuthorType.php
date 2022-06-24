08514b9652176a5120cc59d03608463ed18cc22a
alicekaeva <alikaeva_aa@farpost.com>
2022-06-24 22:33:13 +1000

    nice and clean

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Author;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +14,7 @@ class AuthorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class,['label' => 'Название'])
             ->add('article_id')
         ;
     }
--- src/Form/IssueType.php
+++ src/Form/IssueType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Issue;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,24 @@ class IssueType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('month')
+            ->add('name', TextType::class,['label' => 'Название'])
+            ->add('month', ChoiceType::class, [
+            'choices' => [
+            'Январь' => 'Январь',
+            'Февраль' => 'Февраль',
+            'Март' => 'Март',
+            'Апрель' => 'Апрель',
+            'Май' => 'Май',
+            'Июнь' => 'Июнь',
+            'Июль' => 'Июль',
+            'Август' => 'Август',
+            'Сентябрь' => 'Сентябрь',
+            'Октябрь' => 'Октябрь',
+            'Ноябрь' => 'Ноябрь',
+            'Декабрь' => 'Декабрь',
+        ],
+        'label' => 'Месяц'
+    ])
         ;
     }
 
